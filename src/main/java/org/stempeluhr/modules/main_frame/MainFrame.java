464fc109b0831ffa3ff121e8e90fc35d0a9724fd
Jannik Ewers <V-Modder@web.de>
2018-04-02 13:35:55 +0200

    Fixed gui problems and missing panel

--- src/main/java/org/stempeluhr/modules/main_frame/MainFrame.java
+++ src/main/java/org/stempeluhr/modules/main_frame/MainFrame.java
@@ -20,6 +20,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
 import javax.swing.border.CompoundBorder;
@@ -30,33 +31,30 @@ import org.stempeluhr.modules.common.ActionPanel;
 import org.stempeluhr.modules.common.Constants;
 import org.stempeluhr.modules.common.ModuleFrame;
 import org.stempeluhr.modules.going.GoingController;
-import org.stempeluhr.repository.ConnectionTestRepository;
 
 public class MainFrame extends ModuleFrame {
 
 	private static final long serialVersionUID = 1L;
 	private static final String ShowHelpCommand = "show_help_command";
-	private static final String Standardtext = "<html><body><div style='text-align: center;color:gray'><span style='font-size:40;'><bold>Zeiterfassung</bold></span><br><br>Terminal 1</div></body></html>";
 
 	private JLayeredPane contentPane;
+	private JPanel mainPanel;
+	private ActionPanel goingPanel;
+	private ActionPanel comingPanel;
+
 	private JLabel btnCome;
 	private JLabel btnLeave;
 	private JLabel lblTime;
 	private Timer timer;
 	private JLabel img_footer;
-	private JLabel lbl_method;
-
-	private ActionPanel goingPanel;
-	private ActionPanel comingPanel;
-	private GoingController goingController;
-	private ComingController comingController;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == this.timer) {
 			SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd.MM.yyyy HH:mm:ss");
 			this.lblTime.setText(sdf.format(new Date()));
-		} else if (e.getActionCommand().equals(Constants.CommandMousePressed)) {
+			return;
+		} else if (e.getActionCommand() != null && e.getActionCommand().equals(Constants.CommandMousePressed)) {
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e1) {
@@ -104,9 +102,9 @@ public class MainFrame extends ModuleFrame {
 	}
 
 	public MainFrame() throws UnknownHostException {
-		setBackground(Color.BLACK);
-		ConnectionTestRepository connectionTestRepository = new ConnectionTestRepository();
-		connectionTestRepository.testDbConnection();
+		// ConnectionTestRepository connectionTestRepository = new
+		// ConnectionTestRepository();
+		// connectionTestRepository.testDbConnection();
 		// if (!test) {
 		// JOptionPane.showMessageDialog(null, "Es konnte keine Verbindung
 		// zu\nFetish-Design hergestellt werden!" + "\n\nVerbindung
@@ -114,31 +112,38 @@ public class MainFrame extends ModuleFrame {
 		// System.exit(0);
 		// }
 
+		this.setBackground(Color.BLACK);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(0, 0, 692, 320);
 		if (!Constants.isDebug) {
 			this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 			this.setUndecorated(true);
+			this.setBounds(0, 0, 700, 380);
 		}
-		this.setBounds(0, 0, 692, 320);
+		this.setTitle("Stempel-Uhr");
+		this.setIconImage(new ImageIcon(this.getClass().getResource("icon175x175.png")).getImage());
 
-		this.comingController = new ComingController();
-		this.goingController = new GoingController();
-
-		this.lbl_method = new JLabel("");
-		this.lbl_method.setText(Standardtext);
-		this.lbl_method.setForeground(Color.WHITE);
-		this.lbl_method.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		this.contentPane = new JLayeredPane();
 		this.contentPane.setBackground(Color.BLACK);
 		this.contentPane.setLayout(new BorderLayout(0, 0));
+		this.contentPane.setPreferredSize(new Dimension(692, 320));
 		this.setContentPane(contentPane);
+
+		this.mainPanel = new JPanel();
+		this.mainPanel.setBackground(Color.BLACK);
+		this.mainPanel.setPreferredSize(new Dimension(692, 320));
+		this.mainPanel.setBounds(0, 0, 692, 320);
+		this.mainPanel.setLayout(new BorderLayout());
+		this.contentPane.add(this.mainPanel);
+		this.contentPane.setLayer(this.mainPanel, 0);
+
 		this.btnCome = new JLabel("");
 		this.btnCome.setBackground(null);
 		this.btnCome.setIcon(new ImageIcon(MainFrame.class.getResource("login-128.png")));
 		this.btnCome.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		this.btnCome.setPreferredSize(new Dimension(150, 40));
 		this.btnCome.addMouseListener(this);
-		this.contentPane.add(btnCome, BorderLayout.WEST);
+		this.mainPanel.add(btnCome, BorderLayout.WEST);
 
 		this.btnLeave = new JLabel("");
 		this.btnLeave.setBackground(null);
@@ -146,7 +151,7 @@ public class MainFrame extends ModuleFrame {
 		this.btnLeave.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		this.btnLeave.setPreferredSize(new Dimension(150, 40));
 		this.btnLeave.addMouseListener(this);
-		contentPane.add(this.btnLeave, BorderLayout.EAST);
+		this.mainPanel.add(this.btnLeave, BorderLayout.EAST);
 
 		this.lblTime = new JLabel("Loading...");
 		this.lblTime.setForeground(Color.WHITE);
@@ -155,22 +160,10 @@ public class MainFrame extends ModuleFrame {
 		this.lblTime.setFont(lblTime.getFont().deriveFont(22f));
 		this.lblTime.setBorder(new CompoundBorder(BorderFactory.createMatteBorder(7, 0, 0, 0, Color.decode("#ff80fe")), new EmptyBorder(0, 0, 30, 0)));
 		this.lblTime.setOpaque(true);
-		this.contentPane.add(lblTime, BorderLayout.NORTH);
+		this.mainPanel.add(lblTime, BorderLayout.NORTH);
 
 		this.timer = new Timer(1000, this);
 		this.timer.start();
-
-		this.comingPanel = new ActionPanel();
-		this.comingPanel.setBounds(0, 0, 692, 320);
-		this.comingPanel.setTitel("Kommen");
-		this.comingPanel.setController(this.comingController);
-		this.contentPane.add(this.comingPanel, -1);
-
-		this.goingPanel = new ActionPanel();
-		this.goingPanel.setBounds(0, 0, 692, 320);
-		this.goingPanel.setTitel("Gehen");
-		this.goingPanel.setController(this.goingController);
-		this.contentPane.add(this.goingPanel, -1);
 
 		this.img_footer = new JLabel("");
 		this.img_footer.addMouseListener(new MouseAdapter() {
@@ -179,13 +172,32 @@ public class MainFrame extends ModuleFrame {
 				MainFrame.this.actionPerformed(new ActionEvent(this, 1, ShowHelpCommand));
 			}
 		});
-
 		this.img_footer.setBackground(Color.BLACK);
 		this.img_footer.setOpaque(true);
-		this.img_footer.setBorder(new EmptyBorder(10, 0, 0, 0));// top,left,bottom,right
-		this.contentPane.add(this.img_footer, BorderLayout.SOUTH);
-		this.img_footer.setIcon(new ImageIcon(new javax.swing.ImageIcon(getClass().getResource("Logo_Transparent_1.png")).getImage().getScaledInstance(610, 80, Image.SCALE_SMOOTH)));
-		this.setTitle("Stempel-Uhr");
-		this.setIconImage(new javax.swing.ImageIcon(getClass().getResource("icon175x175.png")).getImage());
+		this.img_footer.setBorder(new EmptyBorder(10, 0, 0, 0));
+		this.img_footer.setIcon(this.getScaledImage("Logo_Transparent_1.png", 610, 80));
+		this.mainPanel.add(this.img_footer, BorderLayout.SOUTH);
+
+		this.comingPanel = new ActionPanel();
+		this.comingPanel.setBounds(0, 0, 692, 320);
+		this.comingPanel.setTitel("Kommen");
+		this.comingPanel.setController(new ComingController());
+		this.contentPane.add(this.comingPanel);
+		this.contentPane.setLayer(this.comingPanel, 1);
+
+		this.goingPanel = new ActionPanel();
+		this.goingPanel.setBounds(0, 0, 692, 320);
+		this.goingPanel.setTitel("Gehen");
+		this.goingPanel.setController(new GoingController());
+		this.contentPane.add(this.goingPanel);
+		this.contentPane.setLayer(this.goingPanel, 2);
+	}
+
+	private ImageIcon getScaledImage(String resourceName, int width, int height) {
+		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource(resourceName));
+		Image image = imageIcon.getImage();
+		Image newImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+
+		return new ImageIcon(newImage);
 	}
 }
